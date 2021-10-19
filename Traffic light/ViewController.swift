a940d3f2cbbf1ce086a3e1905e4c41e534f36999
Moshkin <Moshkin>
2021-10-19 22:51:12 +0300

    First bad realisation with "gray" color and worst loop Первый вариант с блеклыми цветами и плохим циклом

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myRedLabel: UIView!
+    @IBOutlet weak var myYellowLabel: UIView!
+    @IBOutlet weak var myGreenLabel: UIView!
+    @IBOutlet weak var startButtonText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myRedLabel.layer.cornerRadius = 80
+        myRedLabel.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        myGreenLabel.layer.cornerRadius = 80
+        myGreenLabel.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+        myYellowLabel.layer.cornerRadius = 80
+        myYellowLabel.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
     }
+    var actyveView : Int = 1
+    @IBAction func startButton(_ sender: Any) {
+        startButtonText.setTitle("NEXT", for: .normal)
+       
+        if actyveView == 1 {
+            myRedLabel.backgroundColor = UIColor.red.withAlphaComponent(255)
+            myGreenLabel.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            myYellowLabel.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+            actyveView = 2
+        } else {
+            if actyveView == 2 {
+                myYellowLabel.backgroundColor = UIColor.yellow.withAlphaComponent(255)
+                myGreenLabel.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+                myRedLabel.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+                actyveView = 3
+            } else {
+                myGreenLabel.backgroundColor = UIColor.green.withAlphaComponent(255)
+                myYellowLabel.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+                myRedLabel.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+                actyveView = 1
+            }
 
+        }
 
+    }
+    
 }
 
